74e5447f766772519356e431c4788ae4015e9b26
Elijah <elijahjs1999@gmail.com>
2018-03-09 15:41:06 -0500

    Fixed Array Problem
    Was adding an array to an array when I should have been using another for loop

--- Test.py
+++ Test.py
@@ -1,5 +1,10 @@
 import sys
-input1 = ['01', '0111', '001', '001', '00001', '01111', '01', '000011', '0001', '01']
+
+original = [6, 16, 2, 7, 4]
+input1 = ['01', '0111', '001', '001', '00001',]
+"""
+          '01111', '01', '000011', '0001', '01', '0111', '0011', '01', '000011', '01', '01111', '0111', '00001', '00001', '0111', '001', '001', '000111', '000011', '01', '01111', '0111', '0111', '00111', '0011', '001', '01', '011', '000011', '01111', '0001', '0111', '001', '011', '01111', '0000111', '0011', '00001', '011', '0011', '01', '01', '01', '01111', '001', '0111', '0001', '00011', '00111', '011', '00001', '01111', '00011', '01', '00111', '00001', '001', '00001', '00001', '011', '011', '01', '01', '01', '01', '0001', '0111', '0001', '0111', '000111', '00001', '011', '011', '0111', '000111', '01111', '0000111', '00001', '0011', '0000111', '00001', '01', '001', '0001']
+          """
 firstNum = {'01': 1, '001': 2, '0001': 3, '00001': 4}
 secondNum = {'01': 6, '001': 7, '0001': 8, '00001': 9}
 problemNumbers = {'01', '001', '0001', '00001'}
@@ -38,13 +43,16 @@ def P(input2):
         idx = min(int(s) for s in idx)
         # let endPossibilities  = [P(input1[idx + 1 .. n])]
         endPossibilities = [P(input2[idx + 1:])]
-        for possibility in endPossibilities:
-            possibilities.append(convertFromEM(input2[:idx]) + [firstNum[input2[idx]]] + possibility)
-            possibilities.append(convertFromEM(input2[:idx]) + [secondNum[input2[idx]]] + possibility)
+        for possibilityArray in endPossibilities:
+            for possibility in possibilityArray:
+                possibilities.append(convertFromEM(input2[:idx]) + [firstNum[input2[idx]]] + possibility)
+                possibilities.append(convertFromEM(input2[:idx]) + [secondNum[input2[idx]]] + possibility)
 
     return possibilities
 
 sys.setrecursionlimit(10000)
 p = P(input1)
+correctPos = p.index(original)
 
 print("END", p)
+
